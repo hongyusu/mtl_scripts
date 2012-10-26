8586d37f5780c470a8472449e9d8e939a077e998
hongyusu <hongyu.su.uh@gmail.com>
2012-10-26 15:06:17 +0300

    2610

--- autorun_MTL.sh
+++ autorun_MTL.sh
@@ -3,6 +3,6 @@
 #for inname in 'emotions' 'yeast' 'scene' 'enron' 'cal500' 'fp' 'cancer' 'medical' 'toy10' 'toy50'
 for inname in 'toy10'
 do
-    nohup matlab -nodesktop -nosplash -r 'run_MTL.m $inname' $
+    nohup matlab -nodesktop -nosplash -r "run_MTL '$inname'" &
 done
 wait
